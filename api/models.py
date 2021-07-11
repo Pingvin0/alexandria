4502371cf2d4350dae8747457dd23d2e3b87c9d3
Bolubás Marcell <pingvinmagas@gmail.com>
2021-07-11 11:45:53 +0200

    Work on the DB Models

--- api/models.py
+++ api/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
+User = get_user_model()
 # Create your models here.
+
+
+class ApiKey(models.Model):
+    created = models.DateTimeField(auto_now=True)
+    key = models.CharField(max_length=32)
+    # TODO: Many-To-Many access on the data categories
--- data/models.py
+++ data/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
+User = get_user_model()
 # Create your models here.
+
+
+class DataCategory(models.Model):
+    created = models.DateTimeField('created', auto_now=True)
+    creator = models.ForeignKey(User, models.CASCADE)
+    type = models.CharField(max_length=8)
+    
+class DataRecord(models.Model):
+    created = models.DateTimeField('created', auto_now=True)
+    value = models.TextField('value')
+    category = models.ForeignKey(DataCategory, models.CASCADE)
